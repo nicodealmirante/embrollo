3a3dbea593dda4f9a8396002f5a74857db10bd16
Nicolas Luna <nicolas.e.luna@gmail.com>
2026-04-11 16:39:36 +0000

    File changes

--- src/components/ui/use-toast.jsx
+++ src/components/ui/use-toast.jsx
@@ -134,6 +134,8 @@ function toast({ ...props }) {
     },
   });
 
+  setTimeout(() => dismiss(), props.duration ?? 4000);
+
   return {
     id,
     dismiss,
@@ -161,4 +163,4 @@ function useToast() {
   };
 }
 
-export { useToast, toast }; 
+export { useToast, toast };
