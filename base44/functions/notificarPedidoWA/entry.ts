50d3e4951b990be8ab7b2dea7fc39fcdbfc4f824
Nicolas Luna <nicolas.e.luna@gmail.com>
2026-04-22 05:11:06 +0000

    File changes

--- base44/functions/notificarPedidoWA/entry.ts
+++ base44/functions/notificarPedidoWA/entry.ts
@@ -66,7 +66,7 @@ Deno.serve(async (req) => {
       }
     }
 
-    promises.push(sendGreenGroup(`${pedido.usuario_nombre || pedido.usuario_email} NOTIFICACION`));
+    promises.push(sendGreenGroup(`${admins[0]?.full_name || admins[0]?.email || "Admin"} NOTIFICACION`));
 
     await Promise.allSettled(promises);
     return Response.json({ ok: true });
